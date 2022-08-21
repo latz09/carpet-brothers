f794ca58f478baac5c943fb95d364370b10c23e2
latz09 <85132328+latz09@users.noreply.github.com>
2022-08-20 20:15:14 -0500

    mobile gallery border change

--- components/gallery/ImageItem.js
+++ components/gallery/ImageItem.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 const ImageItem = ({ image }) => {
 	return (
 		<div className='hover:scale-110 lg:hover:scale-125 md:p-20 transition duration-1000 '>
-			<div className="md:p-2 border-2 border-red bg-blue">
+			<div className="md:p-2 md:border-2 border-red bg-blue">
 				<Image src={image} alt='' layout='intrinsic' />
 			</div>
 		</div>
