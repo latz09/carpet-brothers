ee6d26f93456cd64c33b0e9f4b812411c37b4f6a
latz09 <85132328+latz09@users.noreply.github.com>
2022-08-20 20:25:02 -0500

    added more images for gallery

--- components/gallery/ImageItem.js
+++ components/gallery/ImageItem.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 const ImageItem = ({ image }) => {
 	return (
 		<div className='hover:scale-110 lg:hover:scale-125 md:p-20 transition duration-1000 '>
-			<div className="md:p-2 md:border-2 border-red bg-blue">
+			<div className="pb-1 bg-blue md:border-2 border-blue grid items-center shadow-xl">
 				<Image src={image} alt='' layout='intrinsic' />
 			</div>
 		</div>
--- data/galleryImages.js
+++ data/galleryImages.js
@@ -1,7 +1,10 @@
 import image1 from '../public/images/carpet-and-vinyle-wood.jpg';
 import image2 from '../public/images/carpet.jpg';
 import image3 from '../public/images/kitchen.jpg';
+import image7 from '../public/images/stairs-2.jpg';
 import image4 from '../public/images/office.jpg';
 import image5 from '../public/images/repair.jpg';
+import image6 from '../public/images/stairs-1.jpg';
+import image8 from '../public/images/step-down.jpg';
 
-export const galleryImages = [image1, image2, image3, image4, image5];
+export const galleryImages = [image1, image6, image3, image4, image5,image7, image2, image8];
