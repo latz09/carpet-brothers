e9e5b49db029133d276f2d8a40146aafc91459eb
latz09 <85132328+latz09@users.noreply.github.com>
2022-08-20 20:16:48 -0500

    changed gap of gallery images

--- components/gallery/Gallery.js
+++ components/gallery/Gallery.js
@@ -5,7 +5,7 @@ import { galleryImages } from '../../data/galleryImages';
 const Gallery = () => {
 	return (
 		<div className=' max-w-7xl mx-auto px-2 md:px-4 '>
-			<div className='grid xl:grid-cols-2 place-items-center  gap-4  bg-landscape-500 py-9'>
+			<div className='grid xl:grid-cols-2 place-items-center  gap-8 md:gap-4  bg-landscape-500 py-9'>
 				{galleryImages.map((image) => (
 					<div key={uuid()}>
 						<ImageItem image={image} />
